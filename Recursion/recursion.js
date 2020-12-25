4c7e91f6d4c6ea5d0c8ddd418d8d09a3193c67f0
Vijendra-Achar <vijendra.1995@gmail.com>
2020-12-25 18:04:43 +0530

    Recursion Problems using Pure Recursion ✔

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -28,5 +28,32 @@ const collectAllOdds = (arr) => {
   return res;
 };
 
-// Test Arrays
+// Test Array
 console.log(collectAllOdds([2, 3, 6, 5, 8, 9, 9, 8, 0, 7, 4]));
+
+// The Above Problem solved using the Pure recursion method
+
+const collectAllOddsPure = (arr) => {
+  // Initilize resultant array / will be empty for every single call
+  let newArr = [];
+
+  // Check if the input array is empty
+  if (arr.length === 0) {
+    return newArr;
+  }
+
+  // Check if the number is odd
+  if (arr[0] % 2 !== 0) {
+    // Push the odd number to the resultant array
+    newArr.push(arr[0]);
+  }
+
+  // Conacatinate all the arrays upon return and assign it to the resultant array
+  newArr = newArr.concat(collectAllOddsPure(arr.slice(1)));
+
+  // Return the Array
+  return newArr;
+};
+
+// Test Array
+console.log(collectAllOddsPure([55, 44, 67, 98, 76, 54, 3]));
